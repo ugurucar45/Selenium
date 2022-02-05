5375a32b42955ebc54639ebd0388daa8a667b9f1
ugurucar45 <ugurucar45@gmail.com>
2022-02-05 14:13:26 -0600

    02/05

--- src/main/java/SeleniumIntro/PracticeSeleniumBasics.java
+++ src/main/java/SeleniumIntro/PracticeSeleniumBasics.java
@@ -9,9 +9,9 @@ public class PracticeSeleniumBasics {
 
         WebDriver driver= new ChromeDriver();
         System.setProperty("webdriver.chrome.driver","chromedriver.exe");
-        //TASK: I want you go to to the 3 websites. get the title,currentURL
+        //TASK: I want you go to the 3 websites. get the title,currentURL
         //You decide and going back
-        //gorward with them lastly refresh and quit.
+        //Forward with them lastly refresh and quit.
 
         driver.navigate().to("https://www.google.com/");
         System.out.println(driver.getTitle());
