915baa936497625a171508d64b861232d28149f7
ugurucar45 <ugurucar45@gmail.com>
2022-02-05 12:30:36 -0600

    02/05

---
+++ src/main/java/SeleniumIntro/SeleniumBasics.java
@@ -0,0 +1,12 @@
+package SeleniumIntro;
+
+public class SeleniumBasics {
+    public static void main(String[] args) {
+//Step1: We need to define the chrome driver into the project as a property.
+//        System.setProperty("webdriver.chrome.driver","chromedriver"); // mac users
+        System.setProperty("webdriver.chrome.driver","chromedriver.exe");//windows users
+
+
+
+    }
+}
